9279ee0c58a83940e77b427486b6224da853710f
myself-viishal <3953vishal@gmail.com>
2024-03-04 19:09:40 +0530

    completed todo list app

---
+++ 10todowithlocalstorage/src/components/Background/Background.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import "./background.css";
+
+export default function Background(){
+  return (
+    <>
+      <div class="wrapper">
+        <div>
+          <span class="dot"></span>
+        </div>
+        <div>
+          <span class="dot"></span>
+        </div>
+        <div>
+          <span class="dot"></span>
+        </div>
+        <div>
+          <span class="dot"></span>
+        </div>
+        <div>
+          <span class="dot"></span>
+        </div>
+        <div>
+          <span class="dot"></span>
+        </div>
+        <div>
+          <span class="dot"></span>
+        </div>
+        <div>
+          <span class="dot"></span>
+        </div>
+        <div>
+          <span class="dot"></span>
+        </div>
+        <div>
+          <span class="dot"></span>
+        </div>
+        <div>
+          <span class="dot"></span>
+        </div>
+        <div>
+          <span class="dot"></span>
+        </div>
+        <div>
+          <span class="dot"></span>
+        </div>
+        <div>
+          <span class="dot"></span>
+        </div>
+        <div>
+          <span class="dot"></span>
+        </div>
+      </div>
+    </>
+  );
+}
